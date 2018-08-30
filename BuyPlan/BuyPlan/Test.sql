5d66065762c3da1aef1c15525587418ac5df8f86
GAPINC\Ptallur <pavan_kumar_talluri@gap.com>
2018-08-30 14:24:16 +0530

    Fisrt Code

---
+++ BuyPlan/BuyPlan/Test.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Test]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+	[LastName] NVARCHAR(50) NULL,
+    [Department] NVARCHAR(50) NULL,
+	[EMail] NVARCHAR(20)
+)
---
+++ BuyPlan/BuyPlan/TestView.sql
@@ -0,0 +1,2 @@
+﻿CREATE VIEW [dbo].[TestView]
+	AS SELECT ID,Name,LastName,Department FROM [Test]
